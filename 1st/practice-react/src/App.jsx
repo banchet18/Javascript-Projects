2e07acdc0323fe3a8dbda96d973e50eebbb09b28
banchet18 <banjarac6@gmail.com>
2024-01-22 19:52:19 +0530

    C

--- 1st/practice-react/src/App.jsx
+++ 1st/practice-react/src/App.jsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
+  const [newData, setNewData] = useState();
+
+  useEffect(() => {
+    const getQuotes = async () => {
+      const response = await fetch("https://type.fit/api/quotes");
+      const data = await response.json();
+      setNewData(data);
+    };
+    getQuotes();
+  }, []);
+  console.log(newData);
   return (
     <>
       <div>
