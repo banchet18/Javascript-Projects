cc673288aa8a6698ecd7d09a0d6f0ce57b71900e
banchet18 <banjarac6@gmail.com>
2024-01-22 19:43:29 +0530

    c

---
+++ 1st/practice-react/src/App.jsx
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <i>{`" `}</i>
+          <span>Some quote</span>
+        </div>
+        <div>author</div>
+
+        <div>
+          <button>twitter</button>
+          <button>next</button>
+        </div>
+      </div>
+    </>
+  );
+};
+export default App;
