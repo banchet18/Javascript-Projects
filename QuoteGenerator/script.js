d7fc4fe351c24710d9fa1098ac60872309b9a43b
banchet18 <banjarac6@gmail.com>
2024-01-22 18:38:57 +0530

    c

--- QuoteGenerator/script.js
+++ QuoteGenerator/script.js
@@ -1,4 +1,3 @@
-const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
