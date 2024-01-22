83ce8071ee2982d246c547f7f88eb0b5ad485afc
banchet18 <banjarac6@gmail.com>
2024-01-22 18:29:46 +0530

    c

--- QuoteGenerator/script.js
+++ QuoteGenerator/script.js
@@ -9,6 +9,21 @@ let Quotes = [];
 //show new quote
 function newQuotes() {
   const Quote = Quotes[Math.floor(Math.random() * Quotes.length)];
+  console.log(Quote);
+
+  //check if author field is blank and replace it with 'unknown'
+  if (!Quote.author) {
+    authorText.textContent = "UnKnown";
+  } else {
+    authorText.textContent = Quote.author;
+  }
+  //check Quote length to determine styling
+  if (Quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = Quote.text;
 }
 
 async function getQuotes() {
