c802391fb86a08ac4c305678eb7f61591254937d
banchet18 <banjarac6@gmail.com>
2024-01-22 17:41:45 +0530

    c

---
+++ QuoteGenerator/script.js
@@ -0,0 +1,15 @@
+let apiQuotes = [];
+
+//Get Quotes from Api
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    console.log(apiQuotes[12]);
+  } catch (error) {
+    //Catch Error Here
+  }
+}
+//On Load
+getQuotes();
