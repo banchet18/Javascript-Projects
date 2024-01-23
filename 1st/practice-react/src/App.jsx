bc5a40df4dacfcff83f6878760ee41f5af2d2471
banchet18 <banjarac6@gmail.com>
2024-01-23 12:23:06 +0530

    c

--- 1st/practice-react/src/App.jsx
+++ 1st/practice-react/src/App.jsx
@@ -1,16 +1,23 @@
 import { useEffect, useState } from "react";
 
 const App = () => {
-  const [newData, setNewData] = useState("");
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const getQuotes = async () => {
+    const response = await fetch("https://type.fit/api/quotes");
+    const data = await response.json();
+    const newText = data[Math.floor(Math.random() * data.length)];
+    setText(newText.text);
+    setAuthor(newText.author);
+  };
 
   useEffect(() => {
     getQuotes();
   }, []);
-  const getQuotes = async () => {
-    const response = await fetch("https://type.fit/api/quotes");
-    const arrayData = await response.json();
-    const data = arrayData[Math.floor(Math.random() * arrayData.length)];
-    setNewData(data);
+
+  const handleClick = () => {
+    getQuotes();
   };
 
   return (
@@ -18,13 +25,12 @@ const App = () => {
       <div>
         <div>
           <i>{`" `}</i>
-          <span>{newData.text}</span>
+          <span>{text}</span>
         </div>
-        <div>{newData.author}</div>
-
+        <div>{author}</div>
         <div>
           <button>twitter</button>
-          <button onClick={() => getQuotes}>next</button>
+          <button onClick={handleClick}>next</button>
         </div>
       </div>
     </>
